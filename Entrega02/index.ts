59c003c1d8cb4e7b85a55a84c7cb8b2b629c454f
Nicolas Di Pietro Paolo <dipietronicolas@gmail.com>
2021-06-09 13:30:24 -0500

    Entrega02

--- Entrega02/index.ts
+++ Entrega02/index.ts
@@ -1,19 +1,32 @@
-const operacion = (num1: number, num2: number, op: string) => {
+const operacion = async (num1: number, num2: number, op: string) => {
+
+  const { Suma } = await import('./Suma.js');
+  const { Resta } = await import('./Resta.js');
+  
   return new Promise<number>((resolve, reject) => {
     if(op === "suma"){
-      resolve(num1 + num2);
+      const e = new Suma(num1, num2);
+      resolve(e.resultado());
     } else if (op === "resta") {
-      resolve(num1 - num2);
+      const e = new Resta(num1, num2);
+      resolve(e.resultado());
     } else {
       reject("Not a valid operation");
     }
   })
 }
 
+
 const operaciones = () => {
+  // Llamada a operacion suma
   operacion(2, 3, "suma").then(result => console.log(result)).catch(e => console.log(e));
-  
+
+  // Llamada a operacion resta
   operacion(7, 3, "resta").then(result => console.log(result)).catch(e => console.log(e));
-  
+
+  // Llamada a operacion con error
+  operacion(7, 3, "sumacion").then(result => console.log(result)).catch(e => console.log(e));
 }
+
+
 operaciones();
