52dc164e2878c3b12952ae6dce77271748217eff
Nicolas Di Pietro Paolo <dipietronicolas@gmail.com>
2021-06-23 11:42:45 -0300

    Entrega06

---
+++ Entrega06/Archivo.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+class Archivo {
+  constructor(file_name) {
+    this.file_name = file_name;
+  }
+  // Funcion que lee el archivo
+  leer = async () => {
+    try {
+      const data = await fs.promises.readFile(`./Entrega06/${this.file_name}`, 'utf-8');
+      console.log(eval(data));
+    } catch (error) {
+      console.log([]);
+    }
+  }
+  // Funcion que guarda en el archivo
+  guardar = async (title, price, thumbnail) => {
+    try {
+
+      const raw_data = await fs.promises.readFile(`./Entrega06/${this.file_name}`, 'utf-8');
+      const data = eval(raw_data);
+
+      const arr_data = Array.from(data);
+      arr_data.push({
+        title, price, thumbnail, id: data.length + 1
+      })
+      await fs.promises.writeFile(`./Entrega06/${this.file_name}`, JSON.stringify(arr_data))
+    } catch (error) {
+      await fs.promises.writeFile(`./Entrega06/${this.file_name}`, JSON.stringify([{
+        title, price, thumbnail, id: 1
+      }]))
+    }
+
+  }
+  // Funcion que borra el archivo
+  borrar = async () => {
+    await fs.promises.unlink(`./Entrega06/${this.file_name}`);
+  }
+}
+
+module.exports = Archivo;
---
+++ Entrega06/index.js
@@ -0,0 +1,20 @@
+// correr con npm run dev06
+
+const Archivo = require('./Archivo');
+const archivo = new Archivo('Entrega.txt');
+
+const llamadas = async () => {
+  // Funciones que escribe el archivo
+  await archivo.guardar('Frasco', 500, './assets/img01.jpg');
+  await archivo.guardar('Mesa', 8000, './assets/img02.jpg');
+  await archivo.guardar('Botella', 600, './assets/img03.jpg');
+
+  // Funcion que lee el arhivo
+  await archivo.leer();
+}
+
+llamadas();
+
+// Funcion que borra el archivo, descomentar para borrar
+//archivo.borrar();
+
