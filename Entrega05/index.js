3dc895089ff79040bdd7db8c82a9560f2be3c8f2
Nicolas Di Pietro Paolo <dipietronicolas@gmail.com>
2021-06-24 09:14:23 -0300

    Subo Entrega05

--- Entrega05/index.js
+++ Entrega05/index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     const myObject = {
         id: randomNumber,
         title: `Producto: ${randomNumber}`,
-        price: (Math.random() * [10, 100, 1000, 10000][parseInt(Math.random() * 3) + 1]).toFixed(2),
+        price: (Math.random() * [10, 100, 1000, 10000][parseInt(Math.random() * 5)]).toFixed(2),
         thumbnail: `Foto ${randomNumber}`
     }
     res.end(`<h1>${JSON.stringify(myObject)}</h1>`);
