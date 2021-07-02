2ff83f12dfa7557ecce96c4026fa48d15f6bd025
Nicolas Di Pietro Paolo <dipietronicolas@gmail.com>
2021-07-01 22:09:43 -0300

    Entrega09 en proceso

--- Entrega09/index.js
+++ Entrega09/index.js
@@ -6,6 +6,14 @@ app.set('PORT', process.env.PORT || 8080);
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('todo bien')
+})
 
 app.listen(app.get('PORT'), () => {
   console.log(`Listening on port ${app.get('PORT')}`);
