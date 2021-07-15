00cd92d3923b6bfcbf0202a118a270dc5b8f8912
Nicolas Di Pietro Paolo <dipietronicolas@gmail.com>
2021-07-15 10:00:28 -0300

    Entrega 11 Terminada

---
+++ Entrega11EJS/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { router } from './routes/routes.js';
+
+const app = express();
+
+// Settings
+app.set('PORT', process.env.PORT || 8080);
+// Estableciendo el directorio donde se encuentran los archivos de plantillas
+app.set("views", "./views");
+// Estableciendo el motor de plantilla que se utiliza, en este caso EJS
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(router);
+
+app.listen(app.get('PORT'), () => {
+  console.log(`Listening on port ${app.get('PORT')}`);
+})
+
+app.on("error", error => console.log(`Error del servidor ${error}`));
