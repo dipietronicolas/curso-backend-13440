a0813026e1e54b55f9dfe3493c9f04f0fbb872e7
Nicolas <dipietronicolas@gmail.com>
2021-08-10 15:45:02 -0300

    Entrega Intermedia 01 terminada

--- EntregaIntermedia01/index.js
+++ EntregaIntermedia01/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+// Variable booleana que simula ser un administrador.
+const isAdministrator = false;
 const productosRouter = require('./routes/productos.routes');
 const carritoRouter = require('./routes/carrito.routes');
 
-// Settings
+// Settings-
 app.set('PORT', process.env.PORT || 8080);
 
+// Custom Middleware que indica si el usuario es admin o no
+const checkLog = (req, res, next) => {
+  req.isAdministrator = isAdministrator;
+  next();
+}
+
 // Middlewares
+app.use(checkLog);
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- EntregaIntermedia01/routes/productos.routes.js
+++ EntregaIntermedia01/routes/productos.routes.js
@@ -15,23 +15,44 @@ router.get('/listar/:id?', async (req, res) => {
 router.post('/agregar', async (req, res) => {
   // Enviados por postman, como campos 
   // x-www-form-urlencoded
-  const { title, description, price, stock, thumbnail } = req.body;
-  const result = await productos.guardar(
-    title, description, price, stock, thumbnail
-  );
-  res.json(result);
+  if (!req.isAdministrator) {
+    res.json({ 
+      error: -1, 
+      descripcion: 'ruta /productos/agregar metodo POST no autorizada' 
+    })
+  } else {
+    const { title, description, price, stock, thumbnail } = req.body;
+    const result = await productos.guardar(
+      title, description, price, stock, thumbnail
+    );
+    res.json(result);
+  }
 })
 
 router.put('/actualizar/:id', async (req, res) => {
-  const result = await productos.modificar({
-    id: Number(req.params.id), ...req.body
-  });
-  res.json(result);
+  if (!req.isAdministrator) {
+    res.jsonres.json({ 
+      error: -1, 
+      descripcion: 'ruta /productos/actualizar/:id metodo PUT no autorizada' 
+    })
+  } else {
+    const result = await productos.modificar({
+      id: Number(req.params.id), ...req.body
+    });
+    res.json(result);
+  }
 })
 
 router.delete('/borrar/:id', async (req, res) => {
-  const result = await productos.eliminarProducto(req.params.id);
-  res.json(result);
+  if (!req.isAdministrator) {
+    res.jsonres.json({ 
+      error: -1, 
+      descripcion: 'ruta /productos/borrar/:id metodo DELETE no autorizada' 
+    })
+  } else {
+    const result = await productos.eliminarProducto(req.params.id);
+    res.json(result);
+  }
 })
 
 module.exports = router;
